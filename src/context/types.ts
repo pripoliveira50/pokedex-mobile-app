a012ddf7bef774d57b3cb635dd8b0c41c101de15
Priscila Oliveira <priscilaoliveira@Priscilas-MacBook-Pro.local>
2023-02-25 19:16:34 -0300

    limpeza projeto

--- src/context/types.ts
+++ src/context/types.ts
@@ -1,41 +1,16 @@
-import { PokemonDetailProps, TypeName } from '@screens/PokemonDetails/types';
 import { ReactNode } from 'react';
-import { UseQueryResult } from 'react-query';
-
-//PokemonList
-export interface PokemonProviderProps {
-  children: ReactNode;
-}
-
-export interface IPokemonsContextData {
-  pokemons: UseQueryResult<any[], unknown>;
-  getMorePokemons: () => void;
-  pokemonList: DataPokemonProps[];
-}
-
-//Details
-export interface PokemonProviderDetailsProps {
-  children: ReactNode;
-}
-
-export interface IPokemonsDetailsContextData {
-  getPokemonDetails(pokemonId: string): Promise<void>;
-  pokemonDetails: PokemonDetailProps;
-  load: boolean;
-  setLoad: (value: boolean) => void;
-}
 
 //Favorites
 export interface FavoritesProviderProps {
   children: ReactNode;
 }
-
 export interface IFavoritesContextData {
   setStorage(value: DataPokemonProps): Promise<void>;
   getStorage(): Promise<void>;
   pokemonList: DataPokemonProps[];
 }
 
+//Types Pokemon
 export type DataPokemonProps = {
   name: string;
   id: number;
@@ -46,11 +21,4 @@ export type PokemonType = {
   type: {
     name: string;
   };
-};
-
-export type PokemonPayloadProps = {
-  name: string;
-  id: number;
-  types: PokemonType[];
-  url: string;
 };
--- src/hooks/useTestProviders/index.tsx
+++ src/hooks/useTestProviders/index.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from '@routes/root';
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
-import { PokemonProvider } from '@context/PokemonContext';
 import { FavoritesProvider } from '@context/FavoritesContext';
 
 interface IProviderProps {
@@ -23,31 +22,21 @@ jest.mock('@react-native-async-storage/async-storage', () =>
 export const useFavoriteProvider: React.FC<IProviderProps> = ({ children }) => (
   <ThemeProvider theme={theme}>
     <FavoritesProvider>
-      <PokemonProvider>
-        <NavigationContainer ref={navigationRef}>
-          {children}
-        </NavigationContainer>
-      </PokemonProvider>
+      <NavigationContainer ref={navigationRef}>{children}</NavigationContainer>
     </FavoritesProvider>
   </ThemeProvider>
 );
 
 export const useHomeProvider: React.FC<IProviderProps> = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <PokemonProvider>
-      <NavigationContainer ref={navigationRef}>{children}</NavigationContainer>
-    </PokemonProvider>
+    <NavigationContainer ref={navigationRef}>{children}</NavigationContainer>
   </ThemeProvider>
 );
 
 export const useCardProvider: React.FC<IProviderProps> = ({ children }) => (
   <ThemeProvider theme={theme}>
     <FavoritesProvider>
-      <PokemonProvider>
-        <NavigationContainer ref={navigationRef}>
-          {children}
-        </NavigationContainer>
-      </PokemonProvider>
+      <NavigationContainer ref={navigationRef}>{children}</NavigationContainer>
     </FavoritesProvider>
   </ThemeProvider>
 );
@@ -57,11 +46,7 @@ export const usePokemonDetailsProvider: React.FC<IProviderProps> = ({
 }) => (
   <ThemeProvider theme={theme}>
     <FavoritesProvider>
-      <PokemonProvider>
-        <NavigationContainer ref={navigationRef}>
-          {children}
-        </NavigationContainer>
-      </PokemonProvider>
+      <NavigationContainer ref={navigationRef}>{children}</NavigationContainer>
     </FavoritesProvider>
   </ThemeProvider>
 );
