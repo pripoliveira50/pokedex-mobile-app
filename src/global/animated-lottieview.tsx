dfaa158ed63a7b3a752e1a8328f3030fba7c12fc
Priscila Oliveira <poliveira13@hotmail..com>
2024-05-12 17:01:53 -0300

    feat: add new file animated-lottieview from global folder

---
+++ src/global/animated-lottieview.tsx
@@ -0,0 +1,5 @@
+
+import LottieView from "lottie-react-native";
+import { Animated } from "react-native"; //Use reanimated if you want to here
+
+export const AnimatedLottieView = Animated.createAnimatedComponent(LottieView);
